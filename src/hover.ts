725481a01bca0eeb7b56095e6578397c70eca917
Jonathan Yong <yongrenjie@gmail.com>
2023-06-27 15:54:37 +0100

    Add colours to map hover/click popup

--- src/hover.ts
+++ src/hover.ts
@@ -14,39 +14,52 @@ function makeHoverHtml(feat: GeoJSON.Feature,
     compareScenarioName: ScenarioName | null,
     activeFactor: LayerName,
 ) {
+    function makeColoredBlock(color: string) : string {
+        return `<span class="colored-block" style="background-color: ${color}"></span>`
+    }
+
     function makeSig(): string {
         const sig = signatures[feat.properties.sig].name;
+        const sigColor = signatures[feat.properties.sig].color;
         if (compareScenarioName === null) {
-            return `<span class="oa-grid-item strong">${sig}</span>`;
+            return activeFactor === "sig"
+                ? `<span class="oa-grid-item strong">${makeColoredBlock(sigColor)}&nbsp;${sig}</span>`
+                : `<span class="oa-grid-item strong">${sig}</span>`;
         } else {
             const cmpSig = signatures[feat.properties["sig-cmp"]].name;
             if (sig === cmpSig) {
-                return `<span class="oa-grid-item strong">${sig}</span>`;
+                return activeFactor === "sig"
+                    ? `<span class="oa-grid-item strong">${makeColoredBlock(sigColor)}&nbsp;${sig}</span>`
+                    : `<span class="oa-grid-item strong">${sig}</span>`;
             } else {
-                return `<span class="oa-grid-item old-sig">${cmpSig}</span><span class="oa-grid-item strong">&nbsp;&nbsp;↳ ${sig}</span>`;
+                return activeFactor === "sig"
+                    ? `<span class="oa-grid-item old-sig">${cmpSig}</span><span class="oa-grid-item strong">&nbsp;&nbsp;↳ &nbsp;${makeColoredBlock(sigColor)}&nbsp;${sig}</span>`
+                    : `<span class="oa-grid-item old-sig">${cmpSig}</span><span class="oa-grid-item strong">&nbsp;&nbsp;↳ ${sig}</span>`;
             }
         }
     }
     function makeIndi(indi: Indicator): string {
         const val = feat.properties[indi.name];
+        let color: string;
         let valString: string;
         if (compareScenarioName === null) {
             valString = val.toFixed(2);
+            color = feat.properties[`${indi.name}-color`];
         } else {
             const cmpVal = feat.properties[`${indi.name}-cmp`];
             const chg = cmpVal === 0 ? 0 : ((val - cmpVal) / cmpVal) * 100;
-            valString = `${val.toFixed(2)} (${
-                chg >= 0 ? "+" : "−"
-            }${Math.abs(chg).toFixed(1)}%)`;
+            color = feat.properties[`${indi.name}-diff-color`];
+            valString = `${val.toFixed(2)} (${chg >= 0 ? "+" : "−"
+                }${Math.abs(chg).toFixed(1)}%)`;
         }
         return [
             `<span>${indi.short.replace(
                 "accessibility",
                 "access."
             )}</span>`,
-            `<span class="right-align-grid-item ${
-                activeFactor === indi.name ? "strong" : ""
+            `<span class="right-align-grid-item ${activeFactor === indi.name ? "strong" : ""
             }">`,
+            activeFactor === indi.name ? `${makeColoredBlock(color)}&nbsp;` : ``,
             valString,
             `</span>`,
         ].join("");
