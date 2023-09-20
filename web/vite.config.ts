172d0b08ed62ed7690c1da3c9a613960d348ed93
Jonathan Yong <yongrenjie@gmail.com>
2023-09-20 14:31:29 +0100

    Set build target to esnext for top-level await

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -4,6 +4,10 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    build: {
+        // Required for top-level await
+        target: 'esnext',
+    },
     plugins: [svelte(), tsconfigPaths()],
     server: {
         proxy: {
