a6e76f522438679a8f1408c0a65003eda17974a3
yongrenjie <yongrenjie@users.noreply.github.com>
2023-11-22 17:33:24 +0000

    deploy: 0d44579a333bf6056fd7323a08f3990561e49c70

---
+++ dev/assets/pyodide_worker-223c35fb.js
@@ -0,0 +1,1 @@
+function e(){return new Worker("/demoland-web/dev/assets/pyodide_worker-046079a9.js",{type:"module"})}export{e as default};
--- dev/assets/pyodide_worker-4fcb317a.js
+++
@@ -1,1 +0,0 @@
-function e(){return new Worker("/demoland-web/dev/assets/pyodide_worker-1578107c.js",{type:"module"})}export{e as default};
