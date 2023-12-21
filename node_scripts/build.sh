60b2c92ad8ff4d37ef7ea778a2623537ae15636d
Jonathan Yong <yongrenjie@gmail.com>
2023-12-21 11:46:46 +0000

    Add new build scripts

---
+++ node_scripts/build.sh
@@ -0,0 +1,70 @@
+#!/bin/sh
+
+# NOTE: This script is intended to be run via npm, not manually.
+#
+# Usage:
+#
+# For local deployment (run a HTTP server from the /dist directory)
+#     build.sh local <AREA_NAME> 
+# For GitHub Pages main branch deployment
+#     build.sh github <AREA_NAME>
+# For GitHub Pages dev branch deployment
+#     build.sh github-dev <AREA_NAME>
+#
+# <AREA_NAME> defaults to "newcastle" if not given.
+
+function usage {
+    echo "Usage: npm run build <local|github|github-dev> [AREA_NAME]"
+    exit 1
+}
+
+# Consume the first argument
+if [ -z "$1" ]; then
+    usage
+else
+    if [ "$1" = "local" ]; then
+        echo "Building for local deployment"
+    elif [ "$1" = "github" ]; then
+        echo "Building for GitHub Pages deployment"
+    else
+        usage
+    fi
+    BUILD_TARGET=$1
+fi
+shift
+
+# This script needs to then also detect the area name too so that we can put
+# the built files in the appropriate output directory.
+if [ -z "$1" ]; then
+    AREA_NAME="newcastle"
+else
+    AREA_NAME=$1
+fi
+
+# The name of the repository on GitHub. This is required for the base path
+# (otherwise hyperlinks won't work)
+REPOSITORY_NAME="demoland-web"
+
+# We use $@ instead of $AREA_NAME so that in the case where the user does not
+# specify an area, load_area.sh notifies the user that we are defaulting to
+# Newcastle.
+npm run load_area $@
+
+if [ "${BUILD_TARGET}" = "local" ]; then
+    npx vite build \
+        --outDir="dist/${AREA_NAME}" \
+        --base="/${AREA_NAME}"
+elif [ "${BUILD_TARGET}" = "github" ]; then
+    # This allows us to simply upload the entire contents of
+    # /tmp/${REPOSITORY_NAME} to the gh-pages branch.
+    npx vite build \
+        --outDir="/tmp/${REPOSITORY_NAME}/${AREA_NAME}" \
+        --base="/${REPOSITORY_NAME}/${AREA_NAME}"
+elif [ "${BUILD_TARGET}" = "github-dev" ]; then
+    npx vite build \
+        --outDir="/tmp/${REPOSITORY_NAME}/${AREA_NAME}" \
+        --base="/${REPOSITORY_NAME}/dev/${AREA_NAME}"
+fi
+
+# Set up the main page to redirect to Newcastle (this is hardcoded in index.html)
+cp node_scripts/index.html dist/index.html
--- node_scripts/load_area.sh
+++ node_scripts/load_area.sh
@@ -6,6 +6,7 @@
 #
 # NOTE: This script is intended to be run via npm, not manually.
 
+BOLD="\033[1m"
 REDBOLD="\033[1;31m"
 RESET="\033[0m"
 
@@ -20,7 +21,7 @@ else
 fi
 
 if [ -d "areas/$AREA_NAME" ]; then
-    printf "Copying files from areas/$AREA_NAME to src/data/..."
+    printf "Copying files from areas/$AREA_NAME to src/data... "
     mkdir -p src/data
     cp -r areas/$AREA_NAME/* src/data
     printf "done\n"
