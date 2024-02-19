df4539462dbe8f0fad833d51e4ee4436e6456ecb
martinfleis <martinfleis@users.noreply.github.com>
2024-02-19 12:20:12 +0000

    deploy: 7aee0a806bab3469f133329a450a475d75b15810

--- dev/isle_of_wight/assets/pyodide_worker-d94dfa79.js
+++
@@ -1,1 +0,0 @@
-function e(){return new Worker("/demoland-web/dev/isle_of_wight/assets/pyodide_worker-60c5be67.js",{type:"module"})}export{e as default};
---
+++ dev/tyne_and_wear/assets/pyodide_worker-27394862.js
@@ -0,0 +1,1 @@
+function e(){return new Worker("/demoland-web/dev/tyne_and_wear/assets/pyodide_worker-3a4054ad.js",{type:"module"})}export{e as default};
