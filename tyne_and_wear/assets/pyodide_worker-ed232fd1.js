2dee6f17b78c363b148c8f7a9fd88e19dac1a690
martinfleis <martinfleis@users.noreply.github.com>
2024-03-19 11:03:20 +0000

    deploy: a8c9ae1c2c88160f05d450a43e301d4144dd7228

---
+++ tyne_and_wear/assets/pyodide_worker-ed232fd1.js
@@ -0,0 +1,1 @@
+function e(){return new Worker("/demoland-web/tyne_and_wear/assets/pyodide_worker-bc3775e6.js",{type:"module"})}export{e as default};
---
+++ tyne_and_wear_hex/assets/pyodide_worker-4dab3a7a.js
@@ -0,0 +1,1 @@
+function e(){return new Worker("/demoland-web/tyne_and_wear_hex/assets/pyodide_worker-bc3775e6.js",{type:"module"})}export{e as default};
